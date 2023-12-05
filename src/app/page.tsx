edc5c08fd3dcffcc38114ed7439398c4c6bfd1ab
Muhammad Tahir <muhammadtahirbharchoond@gmail.com>
2023-12-05 16:03:21 +0500

    client
    shifted nextjs server to client side app.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client"
 import Image from 'next/image'
+import { CldUploadButton } from 'next-cloudinary';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Gallery app</h1>
+      <CldUploadButton uploadPreset="iaeymlgr" />
     </main>
   )
 }
