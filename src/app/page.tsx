7b91ed927387c4d488b9f8c21c91197c07ce5863
Muhammad Tahir <muhammadtahirbharchoond@gmail.com>
2023-12-05 17:36:28 +0500

    image component
    created simple image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,36 @@
 "use client"
 import Image from 'next/image'
 import { CldUploadButton } from 'next-cloudinary';
+import { CldImage } from 'next-cloudinary';
+import { useState } from "react";
+
+
+type UploadResult = {
+  info: {
+    public_id: string;
+  };
+  event: "success";
+}
 
 export default function Home() {
+  const [imageId, setImageId] = useState("")
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Gallery app</h1>
-      <CldUploadButton uploadPreset="iaeymlgr" />
+      <CldUploadButton
+        onUpload={(result: UploadResult) => {
+          setImageId(result.info.public_id)
+        }}
+        uploadPreset="iaeymlgr" />
+      {imageId && (
+        <CldImage
+          width="400"
+          height="300"
+          src={imageId}
+          sizes="100vw"
+          alt="Description of my image"
+        />
+      )}
     </main>
   )
 }
